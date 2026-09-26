baf551d4b67d3c11bad316a57df12acfc18eeef2
Chukwuemeka Ogbonna <165346188+Emeka45@users.noreply.github.com>
2026-09-26 10:47:07 +0100

    Remove incorrect Netlify music API placeholder

--- functions/api/music.js
+++
@@ -1,5 +0,0 @@
-import { getStore } from '@netlify/blobs';
-
-export default async function handler() {
-  return new Response(JSON.stringify({error:'Cloudflare Worker endpoint required'}), {status:501,headers:{'content-type':'application/json'}});
-}
